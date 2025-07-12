ef07a223fd1b323c47d9768cb57408b656ca7653
Junki Kousaka <sumi@JunkinoMacBook-Air.local>
2025-07-12 15:25:23 +0900

    インデント修正

--- src/basicSyntax/Variables.java
+++ src/basicSyntax/Variables.java
@@ -3,10 +3,10 @@ package basicSyntax;
 public class Variables {
     public static void main(String[] args) {
         int num1 = 100;
-		double num2 =1.2;
-		System.out.println(num1);
-		System.out.println(num2);
-		System.out.println("私の好きな整数は" + num1 + "です。");
-		System.out.println("私の好きな小数は" + num2 + "です。");
+        double num2 =1.2;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("私の好きな整数は" + num1 + "です。");
+        System.out.println("私の好きな小数は" + num2 + "です。");
 	}
 }
