534319e150c7c2e18070df588d86f114fe29274a
Junki Kousaka <sumi@JunkinoMacBook-Air.local>
2025-07-12 16:59:07 +0900

    比較演算子と論理演算子の理解と実装

--- src/basicSyntax/ComparisonLogicalOperators.java
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -1,5 +1,32 @@
 package basicSyntax;
 
 public class ComparisonLogicalOperators {
-    
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		
+		boolean result = (a < b);
+		
+		System.out.println(result);
+		
+		boolean isSunny = true;
+		boolean isWarm = true;
+		
+		boolean andResultA = (isSunny && isWarm);
+		
+		System.out.println(andResultA);
+		
+		int x = 3;
+		int y = 4;
+		
+		boolean andResultB = (x >= 0 && y % 2 == 0);
+		
+		System.out.println(andResultB);
+		
+		boolean hasPermission = false;
+		
+		System.out.println(!hasPermission);
+	}
+
 }
