20db3fa052bd65d3fafa761b527d348e105de474
Junki Kousaka <sumi@JunkinoMacBook-Air.local>
2025-07-13 08:53:34 +0900

    メソッド名の修正

--- src/basicSyntax/MethodPractice.java
+++ src/basicSyntax/MethodPractice.java
@@ -3,38 +3,39 @@ package basicSyntax;
 public class MethodPractice {
 
 	public static void main(String[] args) {
-		int a = 1;
-		int b = 2;
+        
+        int a = 1;
+        int b = 2;
 		
-		int result1 = add(a, b);
-		int result2 = subtract(a, b);
-		int result3 = multiply(a, b);
-		int result4 = divide(a, b);
+        int result1 = add(a, b);
+        int result2 = subtract(a, b);
+        int result3 = multiply(a, b);
+        int result4 = divide(a, b);
 		
-		caluculationResult(result1);
-		caluculationResult(result2);
-		caluculationResult(result3);
-		caluculationResult(result4);		
+        showCaluculationResult(result1);
+        showCaluculationResult(result2);
+        showCaluculationResult(result3);
+        showCaluculationResult(result4);		
+    }
+	
+    public static int add(int x, int y) {
+        return x + y;
+    }
+	
+    public static int subtract(int x, int y) {
+        return x - y;
+    }
+	
+    public static int multiply(int x, int y) {
+        return x * y;
 	}
 	
-	public static int add(int x, int y) {
-		return x + y;
-	}
+    public static int divide(int x, int y) {
+        return x / y;
+    }
 	
-	public static int subtract(int x, int y) {
-		return x - y;
-	}
-	
-	public static int multiply(int x, int y) {
-		return x * y;
-	}
-	
-	public static int divide(int x, int y) {
-		return x / y;
-	}
-	
-	public static void caluculationResult(int result) {
-		System.out.println("計算結果は" + result + "です。");
-	}
+    public static void showCaluculationResult(int result) {
+        System.out.println("計算結果は" + result + "です。");
+    }
 
 }
